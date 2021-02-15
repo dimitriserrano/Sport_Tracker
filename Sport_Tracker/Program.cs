c9fb1634251d9fb8be9dad70d1c1c89759eb3b4e
Dimitri Serrano <dimitri.serrano@ynov.com>
2021-02-15 16:18:58 +0100

    initial commit

---
+++ Sport_Tracker/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sport_Tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("       Yo!!!");
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("Quel est ton nom ? ");
+            Console.WriteLine(string.Empty);
+            string inputname = Console.ReadLine();
+            Console.WriteLine(string.Empty);
+            Console.WriteLine("Quel est ton poids ? ");
+            Console.WriteLine(string.Empty);
+            float inputweight = float.Parse(Console.ReadLine());
+            Console.WriteLine(string.Empty);
+            var User = new User
+            {
+                name = inputname,
+                weight = inputweight,
+                date = DateTime.Today
+            };
+            Console.WriteLine(value: User.name);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(value: User.date.ToString("d"));
+            Console.WriteLine(string.Empty);
+            Console.WriteLine(value: User.weight);
+        }
+    }
+}
---
+++ Sport_Tracker/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sport_Tracker
+{
+    class User
+    {
+        public string name { get; set; }
+        public float weight { get; set; }
+        public DateTime date { get; set; }
+
+        public User()
+        {
+
+        }
+    }
+}
